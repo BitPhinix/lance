364b7aae78ea547653ad7dee8f108d384cf92ac5
albertlockett <albert@lancedb.com>
2024-07-04 17:30:30 -0300

    lint and clippy

--- rust/lance-encoding-datafusion/src/zone.rs
+++ rust/lance-encoding-datafusion/src/zone.rs
@@ -24,7 +24,8 @@ use lance_encoding::{
         ScheduledScanLine, SchedulerContext, SchedulingJob,
     },
     encoder::{
-        encode_batch, CoreFieldEncodingStrategy, EncodedBatch, EncodedBuffer, EncodedColumn, FieldEncoder
+        encode_batch, CoreFieldEncodingStrategy, EncodedBatch, EncodedBuffer, EncodedColumn,
+        FieldEncoder,
     },
     format::pb,
     EncodingsIo,
